7f4b3458f1b6d2e46bed3af97421469aa679df4c
oana oprea <oanamaria.oprea@yahoo.com>
2021-12-23 17:07:22 +0200

    lab 11 okish

--- Oprea_Oana_lab10/ListPage.xaml.cs
+++ Oprea_Oana_lab10/ListPage.xaml.cs
@@ -31,5 +31,20 @@ namespace Oprea_Oana_lab10
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
         }
+
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
     }
 }
